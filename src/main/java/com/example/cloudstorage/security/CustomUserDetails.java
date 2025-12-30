9c68f76af41c13eb606735388c5ec1b9cb3f7cba
apollobing <taras.career@gmail.com>
2025-12-30 21:14:23 +0100

    feat: implement Spring Security authentication

---
+++ src/main/java/com/example/cloudstorage/security/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.example.cloudstorage.security;
+
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+
+    @Getter
+    private final Long id;
+    private final String username;
+    private final String password;
+
+    public CustomUserDetails(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    @NotNull
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    @NotNull
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    @NotNull
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
